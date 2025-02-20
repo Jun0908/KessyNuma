7930a9297464ad36003ee09b57b6207706ed1de1
Jun0908 <31527310+Jun0908@users.noreply.github.com>
2025-02-20 17:19:59 +0900

    Update LiquidDemocracy.sol

--- contract/hardhat/contracts/LiquidDemocracy.sol
+++ contract/hardhat/contracts/LiquidDemocracy.sol
@@ -3,18 +3,19 @@ pragma solidity ^0.8.20;
 
 contract LiquidDemocracy {
     struct Voter {
-        bool voted;
-        address delegate;
-        uint weight;
+        bool voted;     
+        address delegate; 
     }
 
     mapping(address => Voter) public voters;
     mapping(uint => uint) public votes;
     address public owner;
     uint public proposalCount;
+    address[] public voterList; 
 
     event Voted(address indexed voter, uint proposal);
     event Delegated(address indexed from, address indexed to);
+    event VotesReset(address indexed by);
 
     constructor(uint _proposalCount) {
         owner = msg.sender;
@@ -23,32 +24,60 @@ contract LiquidDemocracy {
 
     function vote(uint proposal) public {
         require(proposal < proposalCount, "Invalid proposal");
-        Voter storage sender = voters[msg.sender];
-        require(!sender.voted, "Already voted");
-        
-        sender.voted = true;
-        votes[proposal] += sender.weight + 1;
-        emit Voted(msg.sender, proposal);
+        require(!voters[msg.sender].voted, "Already voted");
+
+        address voter = msg.sender;
+        while (voters[voter].delegate != address(0)) {
+            voter = voters[voter].delegate;
+        }
+
+        if (voters[msg.sender].delegate == address(0)) {
+            voterList.push(msg.sender); 
+        }
+
+        voters[msg.sender].voted = true;
+        votes[proposal] += 1;
+        emit Voted(voter, proposal);
     }
 
     function delegate(address to) public {
         require(to != msg.sender, "Cannot delegate to yourself");
         require(!voters[msg.sender].voted, "Already voted");
-        
-        Voter storage sender = voters[msg.sender];
-        while (voters[to].delegate != address(0)) {
-            to = voters[to].delegate;
-            require(to != msg.sender, "Delegation loop detected");
+
+        address currentDelegate = to;
+        while (voters[currentDelegate].delegate != address(0)) {
+            currentDelegate = voters[currentDelegate].delegate;
+            require(currentDelegate != msg.sender, "Delegation loop detected");
         }
-        
-        sender.voted = true;
-        sender.delegate = to;
-        voters[to].weight += sender.weight + 1;
+
+        if (voters[msg.sender].delegate == address(0)) {
+            voterList.push(msg.sender); 
+        }
+
+        voters[msg.sender].voted = true;
+        voters[msg.sender].delegate = to;
         emit Delegated(msg.sender, to);
     }
 
     function getVotes(uint proposal) public view returns (uint) {
         require(proposal < proposalCount, "Invalid proposal");
         return votes[proposal];
+    }
+
+    function resetVotes() public {
+        require(msg.sender == owner, "Only owner can reset votes");
+
+        for (uint i = 0; i < proposalCount; i++) {
+            votes[i] = 0;
+        }
+
+        for (uint i = 0; i < voterList.length; i++) {
+            address voter = voterList[i];
+            voters[voter].voted = false;
+            voters[voter].delegate = address(0);
+        }
+
+        delete voterList; 
+        emit VotesReset(msg.sender);
     }
 }
